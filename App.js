adfbe14e67504f66cafbd1d9a00b9da4334f9d75
rishabhnhr18 <rishabhnahar18@gmail.com>
2024-08-26 19:01:34 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-Hii Rishabh Nahar
+if(exp==true) 
+{
+    do this thing
+}
+else   // exp=false
+{
+    do this thing
+}
+
+Conditional statements, loop, array, functionalities, functions, methods, collectionc classes, file handing
+
+
+lorem

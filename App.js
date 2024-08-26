ebd8747793060754a723a45208ec5ddd4c37c11e
rishabhnhr18 <rishabhnahar18@gmail.com>
2024-08-26 19:18:25 +0530

    Initial Commmit

--- App.js
+++ App.js
@@ -1,13 +1,1 @@
-if(exp==true) 
-{
-    do this thing
-}
-else   // exp=false
-{
-    do this thing
-}
-
-Conditional statements, loop, array, functionalities, functions, methods, collectionc classes, file handing
-
-
-lorem
+Hello Master Guys, how ita been going onabort.apply.call. Pretty awsome...
